4c33dbb18580dea4170c1b4023eb12d591ce11c7
vincent <vincentlegoff2004@yahoo.fr>
2017-05-31 21:24:12 +0200

    migration to Django 1.11

--- menu/urls.py
+++ menu/urls.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from . import views
 
 
-urlpatterns = patterns('menu.views',
+urlpatterns = [
     #gestion du menu de la semaine
-    url(r'^menu/generer_semaine/$', 'generer_semaine', name="generer_semaine"),
-    url(r'^generer_menu/$', 'generer_menu', name="generer_menu"),
-    url(r'^voir_semaines_precedentes/$', 'liste_semaines_precedentes',
+    url(r'^generer_semaine/', views.generer_semaine, name="generer_semaine"),
+    url(r'^generer_menu/$', views.generer_menu, name="generer_menu"),
+    url(r'^voir_semaines_precedentes/', views.liste_semaines_precedentes,
         name="liste_semaines_precedentes"),
-    url(r'^voir_semaine/(?P<semaine_id>\d+)$', 'voir_semaine',
+    url(r'^voir_semaine/(?P<semaine_id>\d+)', views.voir_semaine,
         name="voir_semaine"),
-    url(r'^editer_profil_semaine$', 'editer_profil_semaine',
+    url(r'^editer_profil_semaine', views.editer_profil_semaine,
         name="editer_profil_semaine"),
-    url(r'modifier_menu/(?P<form_id>\d+)$', 'menu_modifier',
+    url(r'^modifier_menu/(?P<form_id>\d+)', views.menu_modifier,
         name="menu_modifier"),
-    url(r'reediter_menu_semaine/$', 'reediter_menu_semaine',
+    url(r'^reediter_menu_semaine/', views.reediter_menu_semaine,
         name="reediter_menu_semaine"),
     #gestion des recettes
-    url(r'^liste$', 'liste_recette', name="liste_recette"),
-    url(r'^recette/(?P<id>\d+)$', 'voir_detail', name="voir_detail"),
-    url(r'^entrer_recette/$', 'entrer_recette', name="entrer_recette"),
-    url(r'^editer_recette/(?P<id>\d+)$', 'editer_recette',
+    url(r'^liste$', views.liste_recette, name="liste_recette"),
+    url(r'^recette/(?P<id>\d+)', views.voir_detail, name="voir_detail"),
+    url(r'^entrer_recette/', views.entrer_recette, name="entrer_recette"),
+    url(r'^editer_recette/(?P<id>\d+)', views.editer_recette,
         name="editer_recette"),
-    url(r'^recette$', 'recette_aleatoire', name="recette_aleatoire"),
-        )
+    url(r'^recette', views.recette_aleatoire, name="recette_aleatoire"),
+        ]
--- menu_semaine/urls.py
+++ menu_semaine/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from menu import views
 
 urlpatterns = [
-    url(r'^/?$', views.accueil),
+    url(r'^$', views.accueil, name='accueil'),
     url(r'^menu/', include('menu.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
