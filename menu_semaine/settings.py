819b75f4e123bc725bdc04c3c1e34333193de448
vincent <vincentlegoff2004@yahoo.fr>
2017-06-06 22:46:49 +0200

    Minor correction in settings.py

--- menu_semaine/settings.py
+++ menu_semaine/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if prod:
-    pass
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 else:
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static"),
