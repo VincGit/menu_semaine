9518e89392c3881568432241377499aaee92f2e0
vincent <vincentlegoff2004@yahoo.fr>
2017-07-10 22:21:20 +0200

    Modify admin to show date of created weeks

--- menu/admin.py
+++ menu/admin.py
@@ -14,6 +14,11 @@ class IngredientAdmin(admin.ModelAdmin):
     ordering = ('nom',)
 
 
+class SemaineRempliAdmin(admin.ModelAdmin):
+    list_display = ('numero_semaine', 'profil', 'date')
+    ordering = ('date', )
+
+
 admin.site.register(Categorie)
 admin.site.register(Saison)
 admin.site.register(Recette, RecetteAdmin)
@@ -21,4 +26,4 @@ admin.site.register(Repas)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(ReferenceRepas)
 admin.site.register(ReferenceSaison)
-admin.site.register(SemaineRempli)
+admin.site.register(SemaineRempli, SemaineRempliAdmin)
